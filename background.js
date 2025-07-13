12da8d48b6b417cbbbab1729a8149da253f4782d
Felix <fixel99@gmail.com>
2025-07-13 23:59:01 +1000

    UPDATE 1.2 | fix for manifest 3 | 2nd Monitor fix
    2nd monitor fix is manual. To get automatically/add config in next version

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
-// background.js
-
-chrome.action.onClicked.addListener(function () {
-
-  var width = 16500;
-  var left = 1000;
-
-  //left += window.screenX;
-
-  chrome.windows.create({ url: chrome.runtime.getURL("localpage.html"), left:14400, state:"fullscreen"});});
+chrome.action.onClicked.addListener(() => {
+  chrome.windows.create({
+    url: chrome.runtime.getURL("localpage.html"),
+    type: "popup",
+    width: 1500,
+    height: 1000
+  }, (newWin) => {
+    // Delay move until Chrome/OS is ready
+    setTimeout(() => {
+      chrome.windows.update(newWin.id, {
+        left: 2800, // Just into the second monitor
+        top: 400
+      }, () => {
+        if (chrome.runtime.lastError) {
+          console.error("Move failed:", chrome.runtime.lastError.message);
+        } else {
+          console.log("Window moved successfully.");
+        }
+      });
+    }, 1); // tiny delay to ensure the window is ready
+  });
+});

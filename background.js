de6a9bf5ef0ebe23309965ff3214ae1f23b934a0
Felix <fixel99@gmail.com>
2025-07-13 23:37:53 +1000

    Update background.js

--- background.js
+++ background.js
@@ -7,6 +7,4 @@ chrome.action.onClicked.addListener(function () {
 
   //left += window.screenX;
 
-  chrome.windows.create({ url: chrome.runtime.getURL("localpage.html")/*, left:14400/*, state:"fullscreen"*/ });
-
-});
+  chrome.windows.create({ url: chrome.runtime.getURL("localpage.html"), left:14400, state:"fullscreen"});});
